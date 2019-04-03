2fe4117ce23e5fa24d48e5e79ba5e2677d658be9
julien <julien.bordagi@netcourrier.com>
2019-04-03 11:58:33 +0200

    Integgration des variables

--- hello.py
+++ hello.py
@@ -4,9 +4,15 @@
     Store some aithmetics into the var
     Display result
     Operands, operators, functions
+
+    @version 0.0.2
+    Add two operands and replace compute method
 """
 resultat = 0 #Définition de la variable
-resultat = 2 + 2
+
+operand1 = 3
+operand2 = 2
+resultat = operand1 + operand2
 print(resultat)
 
 """

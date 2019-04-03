e9c62de65c191689c2eb6bf92d654105a3fe894c
julien <julien.bordagi@netcourrier.com>
2019-04-03 11:40:41 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+"""
+    Algotithm #0.0.1
+    Declare var
+    Store some aithmetics into the var
+    Display result
+    Operands, operators, functions
+"""
+resultat = 0 #Définition de la variable
+resultat = 2 + 2
+print(resultat)

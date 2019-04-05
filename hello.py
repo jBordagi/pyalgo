e4a8d8c95b0812fba3c625d937032614937b20ad
julien <julien.bordagi@netcourrier.com>
2019-04-05 16:36:28 +0200

    hello

--- hello.py
+++ hello.py
@@ -8,11 +8,22 @@
     @version 0.0.2
     Add two operands and replace compute method
 """
+"""
+function setting
+"""
+def addition(operande1, operande2):
+    if operande1 < 0:
+        operande1 = operande1 * -1
+    if operande2 < 0:
+        operande2 = operande2 * -1
+    return operande1 + operande2
+
 resultat = 0 #Définition de la variable
 
-operande1 = -3
-operande2 = 5
-resultat = operande1 + operande2
+operande1 = -3 #imagine it's the min of an array
+operande2 = 2 #imagine it's the max of an array
+resultat = addition(5, 3) #call addition function with 5 and 3 as params
+print(addition(operande1, operande2))
 print(resultat)
 
 if resultat > 0:
